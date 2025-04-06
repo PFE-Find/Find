8107bc34661946ec9053bd31ffd2c80c693481af
medwessim <wessimsaidani56@gmail.com>
2025-04-06 16:58:05 +0100

    admin commits

--- back/PostService/routes/items.js
+++ back/PostService/routes/items.js
@@ -1,12 +1,15 @@
 import express from 'express';
-import { createItem, getItems, getItemById, updateItem, deleteItem } from '../controllers/itemController.js';
+import { createItem, getItems, getItems1, getItems2, getItemById, updateItem, deleteItem, updateOffreStatus } from '../controllers/itemController.js';
 
 const router = express.Router();
 
 router.post('/', createItem);
 router.get('/GetAll', getItems);
+router.get('/GetAll1', getItems1);
+router.get('/GetAll2', getItems2);
 router.get('/:id', getItemById);
 router.put('/:id', updateItem);
 router.delete('/:id', deleteItem);
+router.patch('/:id', updateOffreStatus);
 
 export default router;
---
+++ front/app/Admin/DashBoard/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+import Dashbord from "../../components/Admin/Dashbord";
+
+
+
+
+export default function OffrePage() {
+    return (
+        <>
+        
+        <Dashbord/>
+        
+        </>
+    );
+}
