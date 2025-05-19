5ac5221e6e14ed872a2e21165f6a5bd6eaff8736
medwessim <wessimsaidani56@gmail.com>
2025-05-19 14:18:03 +0100

    new commit

--- front/next.config.ts
+++ front/next.config.ts
@@ -4,9 +4,7 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  experimental: {
-    reactRefresh: false, // Disables Hot Reload (Fast Refresh)
-  },
+  
 };
 
 export default nextConfig;
